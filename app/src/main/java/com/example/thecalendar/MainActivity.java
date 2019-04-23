505c62e1fbf092513a3e2042e9d5ace08e31d051
ZZL2000 <zz27@illinois.edu>
2019-04-23 15:23:11 -0500

    Initial commit

---
+++ app/src/main/java/com/example/thecalendar/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.thecalendar;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import android.content.Intent;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    @Override
+    public <T extends View> T findViewById(int id) {
+        return super.findViewById(id);
+    }
+    public void onClick(View view)
+    {
+        startActivity(new Intent("myActivityjan"));
+
+    }
+
+}
